daa49fd9d8ecb342f9036871433ae1467c68391c
abilissful <theabigailwilson@gmail.com>
2021-12-06 23:28:00 -0500

    day one complete

---
+++ Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+var dayOne = new DayOne();
+// Console.WriteLine(dayOne.GetIncreasingDepthCount());
+Console.WriteLine(dayOne.GetSlidingIncreasingDepthCount());
